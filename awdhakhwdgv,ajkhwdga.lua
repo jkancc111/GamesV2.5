eadc9382f8a139dde48c281b25e0e759c7670f81
jkancc111 <jkancc3@gmail.com>
2025-07-21 23:03:03 +0700

    Update awdhakhwdgv,ajkhwdga.lua

--- awdhakhwdgv,ajkhwdga.lua
+++ awdhakhwdgv,ajkhwdga.lua
@@ -3,7 +3,7 @@ local function runAppropriateScript()
     local gameId = game.PlaceId
     
     local scripts = {
-        [17534163435] = "https://raw.githubusercontent.com/jkancc111/GamesV2.5/refs/heads/main/DungeonRNG.lua"
+        [17534163435] = "https://raw.githubusercontent.com/jkancc111/GamesV2.5/refs/heads/main/DungeonRNG.lua",
     [124180759222403] = "https://raw.githubusercontent.com/jkancc111/GamesV2.5/main/Ants.lua",
     [109983668079237] = "https://raw.githubusercontent.com/jkancc111/GamesV2.5/main/test.lua",
     }
